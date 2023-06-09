6d3408282368713ee088a536727f6d9d4dcb7570
volunteercjt <volunteercjt@gmail.com>
2023-06-08 19:57:38 -0500

    Finished with all questions and bonus from main assignment.

--- SQL_Scripts/COUNT_data_analyst_jobs_3.sql
+++ SQL_Scripts/COUNT_data_analyst_jobs_3.sql
@@ -29,9 +29,10 @@
 
 -- SELECT COUNT(*)
 -- FROM data_analyst_jobs
--- WHERE star_rating > 4;
+-- WHERE location = 'TN'
+-- 	AND star_rating > 4;
 
---Answer:  416
+--Answer:  3
 
 --5.	How many postings in the dataset have a review count between 500 and 1000?
 
@@ -79,11 +80,44 @@
 
 --10.	Add the code to order the query in #9 from highest to lowest average star rating. Which company with more than 5000 reviews across all locations in the dataset has the highest star rating? What is that rating?
 
-SELECT DISTINCT(company), AVG(star_rating) AS avg_star_rating
-FROM data_analyst_jobs
-WHERE review_count >5000
-	AND company IS NOT NULL
-GROUP BY company
-ORDER BY avg_star_rating DESC
+-- SELECT DISTINCT(company), AVG(star_rating) AS avg_star_rating
+-- FROM data_analyst_jobs
+-- WHERE review_count >5000
+-- 	AND company IS NOT NULL
+-- GROUP BY company
+-- ORDER BY avg_star_rating DESC
 
-Answer:  It's a 6 way tie between American Express, General Motors, Kaiser Permanente, Microsoft, Nike, and Unilever.  Rating: 4.2
+--Answer:  It's a 6 way tie between American Express, General Motors, Kaiser Permanente, Microsoft, Nike, and Unilever.  Rating: 4.2
+
+--11.	Find all the job titles that contain the word ‘Analyst’. How many different job titles are there?
+
+-- SELECT DISTINCT(title)
+-- FROM data_analyst_jobs
+-- WHERE UPPER(title) LIKE '%ANALYST%'
+
+-- Answer:  774
+
+--12.	How many different job titles do not contain either the word ‘Analyst’ or the word ‘Analytics’? What word do these positions have in common?
+
+-- SELECT DISTINCT(title)
+-- FROM data_analyst_jobs
+-- WHERE UPPER (title) NOT LIKE '%ANALYST%'
+-- 	AND UPPER (title) NOT LIKE '%ANALYTICS%'
+	
+-- Answer: 26.  The jobs remaining are either all caps or lowercase.  If you exclude the all caps and lowercase then the positions have Tableau in common.
+
+-- **BONUS:**
+-- You want to understand which jobs requiring SQL are hard to fill. Find the number of jobs by industry (domain) that require SQL and have been posted longer than 3 weeks. 
+--  - Disregard any postings where the domain is NULL. 
+--  - Order your results so that the domain with the greatest number of `hard to fill` jobs is at the top. 
+--   - Which three industries are in the top 4 on this list? How many jobs have been listed for more than 3 weeks for each of the top 4?
+
+-- SELECT domain, COUNT(domain) AS count_days_since_posting
+-- FROM data_analyst_jobs
+-- WHERE skill LIKE '%SQL%'
+-- 	AND days_since_posting > 21
+-- 	AND domain IS NOT NULL
+-- GROUP BY domain
+-- ORDER BY count_days_since_posting DESC
+
+-- Answer:  Internet and Software (62), Banks and Financial Services (61), Consulting and Business Services (57), Health Care (52)
